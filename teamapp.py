2e8b16e0aa769f56527d4a149cab4bd2176928ae
SoxSavant <sambro449@gmail.com>
2025-11-16 23:18:20 -0500

    adjusted PDF padding

--- teamapp.py
+++ teamapp.py
@@ -834,7 +834,7 @@ with right_col:
     ax.axis("off")
 
     pdf_buffer = BytesIO()
-    fig.savefig(pdf_buffer, format="pdf", bbox_inches = "tight", pad_inches = .25)
+    fig.savefig(pdf_buffer, format="pdf", bbox_inches = "tight", pad_inches = .15)
     pdf_buffer.seek(0)
 
     st.pyplot(fig, use_container_width=False, clear_figure=True)
